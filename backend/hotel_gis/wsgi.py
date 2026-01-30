3161c06b144b732d5c1a147a00b82b7d1404d3ff
Anh Ben <anhben0204@gmail.com>
2026-01-30 15:15:39 +0700

    test

---
+++ backend/hotel_gis/wsgi.py
@@ -0,0 +1,13 @@
+"""
+WSGI config for hotel_gis project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hotel_gis.settings')
+
+application = get_wsgi_application()
